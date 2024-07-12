b3ea023d6118ab2edc975a72b17a519f32336de7
Abir Banerjee <abirbanerjee793@gmail.com>
2024-07-12 13:51:22 +0530

    Integrated clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,8 +20,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+        <header>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  </ClerkProvider>
+    
+  );
+}
+
+
+
+{/* <ClerkProvider>
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
     </html>
-  );
-}
+    </ClerkProvider>
+ */}
